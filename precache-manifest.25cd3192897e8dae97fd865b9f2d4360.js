f10e7da4e4e75a3c74677ae171e8978a130f6baf
Kulnois <rasecmen58@hotmail.com>
2020-09-14 15:25:40 -0500

    Updates

---
+++ precache-manifest.25cd3192897e8dae97fd865b9f2d4360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb218d7eb3d34c000593b47932ccb82",
+    "url": "/movies-search-react/index.html"
+  },
+  {
+    "revision": "1f55e68e708026c58f13",
+    "url": "/movies-search-react/static/css/main.a1d7310b.chunk.css"
+  },
+  {
+    "revision": "752539f82bdd10a1150f",
+    "url": "/movies-search-react/static/js/2.49aa0cc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-search-react/static/js/2.49aa0cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f55e68e708026c58f13",
+    "url": "/movies-search-react/static/js/main.24644b13.chunk.js"
+  },
+  {
+    "revision": "57cc473c3fb9a9e594c7",
+    "url": "/movies-search-react/static/js/runtime-main.cb9fbb31.js"
+  }
+]);
